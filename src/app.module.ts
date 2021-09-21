a09888d6e51327693be1915156740f39085403e6
Maximiliano Cortes <max.cortes.f@gmail.com>
2021-09-21 18:11:00 -0300

    Certificados TLS para DigitalOceanç

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { AppointmentModule } from './appointment/appointment.module';
 import { MongooseModule } from "@nestjs/mongoose";
 import { ConfigModule } from '@nestjs/config';
+import { readFileSync } from 'fs';
 
 //import { db } from "../db.const";
 
@@ -11,7 +12,10 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot(),
     AppointmentModule,
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      tls: true,
+      tlsInsecure: true
+    }),
   ],
   controllers: [
     AppController
